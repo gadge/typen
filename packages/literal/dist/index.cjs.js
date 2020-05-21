fd500bb2256a0c10245665863102f9cd8b6ffe6d
alkashi <hoyeungw@outlook.com>
2020-05-21 10:15:33 +0800

    add @typen/literal

--- packages/literal/dist/index.cjs.js
+++ packages/literal/dist/index.cjs.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 var enumDataTypes = require('@typen/enum-data-types');
 
-const LITERAL = /[A-Za-z0-9]+/g;
+const LITERAL = /[A-Za-z0-9]+/;
 const isLiteral = x => LITERAL.test(x);
 const isString = x => typeof x === enumDataTypes.STR;
 
--- packages/literal/dist/index.esm.js
+++ packages/literal/dist/index.esm.js
@@ -1,6 +1,6 @@
 import { STR } from '@typen/enum-data-types';
 
-const LITERAL = /[A-Za-z0-9]+/g;
+const LITERAL = /[A-Za-z0-9]+/;
 const isLiteral = x => LITERAL.test(x);
 const isString = x => typeof x === STR;
 
