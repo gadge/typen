301c0a7ff1a30325fa2e7af39757eb300d2ac8cd
hazen <hoyeungw@outlook.com>
2020-09-01 10:34:48 +0800

    add isNumStr

--- packages/literal/src/literal.js
+++ packages/literal/src/literal.js
@@ -1,6 +1,10 @@
-import { STR } from '@typen/enum-data-types'
+import { NUM, STR } from '@typen/enum-data-types'
 
 export const LITERAL = /[A-Za-z0-9]+/
 export const isLiteral = x => LITERAL.test(x)
 export const isString = x => typeof x === STR
+export const isNumStr = x => {
+  const t = typeof x
+  return t === STR || t === NUM
+}
 export const hasLiteral = x => isString(x) && isLiteral(x)
