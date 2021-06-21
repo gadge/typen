c3f7f774cafa8773a0e7e44764e08df36c93306f
Hoyeung <hoyeungw@outlook.com>
2021-06-21 17:41:39 +0800

    update dependencies

--- packages/nullish/dist/index.cjs.js
+++ packages/nullish/dist/index.cjs.js
@@ -3,5 +3,7 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 
 const nullish = x => x === null || x === void 0;
+const valid = x => x !== null && x !== void 0;
 
 exports.nullish = nullish;
+exports.valid = valid;
--- packages/nullish/dist/index.esm.js
+++ packages/nullish/dist/index.esm.js
@@ -1,3 +1,4 @@
 const nullish = x => x === null || x === void 0;
+const valid = x => x !== null && x !== void 0;
 
-export { nullish };
+export { nullish, valid };
